0cd264d618a68bc00e81ddd241043c8b1de95fa5
AdaZuniga <56266464+AdaZuniga@users.noreply.github.com>
2022-04-18 13:06:31 -0500

    Mongoose intro

---
+++ README.md
@@ -0,0 +1,8 @@
+# ***MONGOOSE***
+
+## ODM 
+OBJECT DATA MAPPER .... OBJECT DOCUMENT MAPPER
+ODMs like Mongoose map documents coming form a database into usable JavaSccript objects.
+
+Mongoose provides ways for us to model out our application data and define a schema. It offers easy ways to validate data and build complex queries from the confort of JS
+
---
+++ index.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+mongoose.connect('mongodb://localhost:27017/movieApp')
+.then(() => {
+    console.log("connection OPEN!");
+})
+.catch(err => {
+    console.log("ERROR" + err);
+})
+
+
+const movieSchema = new mongoose.Schema({
+    title: String,
+    year: Number,
+    score: Number,
+    rating: String
+});
+
+const Movie = mongoose.model("Movie", movieSchema);
+// const nemo = new Movie({title: "Nemo", year: 2000, score: 9.2, rating: "R"});
+
+
+// Movie.insertMany([
+//     {title: "Amelie", year: 2001, score: 8.2, rating: "R"},
+//     {title: "Alien", year: 1979, score: 8.1, rating: "R"},
+//     {title: "The Iron Giant", year: 1999, score: 7.5, rating: "PG"},
+//     {title: "Stand By Me", year: 1986, score: 8.6, rating: "R"},
+//     {title: "Moonrise Kingdom", year: 2012, score: 7.3, rating: "PG-13"}
+// ])
+//     .then(data =>{
+//         console.log("insertMany completed");
+//         console.log(data);
+//     });
+
